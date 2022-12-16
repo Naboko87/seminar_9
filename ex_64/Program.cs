bfce6b28683c887442539d5e24d2f64e9cacf75a
Kseniya <Naboko.kv@gmail.com>
2022-12-17 00:36:41 +0200

    ex

---
+++ ex_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, ex_64!");
+Console.WriteLine("Write start number");
+int start = Convert.ToInt32(Console.ReadLine());
+int finish = 1;
+
+string recursion(int number, int b){
+   
+    if ( number <= b) return recursion(number+1, b) + number;
+    else
+    return String.Empty;
+}
+
+
+
+Console.WriteLine();
+
+Console.WriteLine(recursion(finish, start));
---
+++ ex_68/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, ex_68");
+
+int mm = 2;
+int nn = 3;
+
+
+int akkerman(int m, int n){
+    if (n == 0)
+        return m + 1;
+    else
+      if (m == 0)
+        return akkerman(n - 1, 1);
+    else
+        return akkerman(n - 1, akkerman(n, m - 1));
+}
+    
+    Console.WriteLine(akkerman(mm, nn));
+
+
